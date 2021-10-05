4b17c19baddec11060f59ddd3709a42be82d97d7
zorro901 <zoro907@gmail.com>
2021-10-05 22:57:17 +0900

    32. localStorageで状態の永続化をしよう

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { EventForm } from './EventForm'
@@ -7,9 +7,16 @@ import { OperationLogs } from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
 
+const APP_KEY = 'appWithRedux'
+
 const App = () => {
-  const initialState = { events: [], operationLogs: [] }
+  const appState = localStorage.getItem(APP_KEY)
+  const initialState = appState ? JSON.parse(appState) : { events: [], operationLogs: [] }
   const [state, dispatch] = useReducer(reducer, initialState)
+  
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state))
+  }, [state])
   
   return (
     <AppContext.Provider value={{ state, dispatch }}>
